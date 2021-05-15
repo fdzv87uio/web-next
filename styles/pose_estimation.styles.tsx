79b2b55d10c56868eae17d6f7ad138fdd216eab4
@fausto1 <fausto@parts-badger.com>
2021-05-14 19:50:58 -0500

    initial commit

--- styles/pose_estimation.styles.tsx
+++ styles/pose_estimation.styles.tsx
@@ -5,8 +5,12 @@ export const PageWrapper = Styled.div`
     display: block;
     margin: auto;
     background-color: #FFD733;
-    width:100%;
+    width:100vh;
     height:900px;
-
+    
+    video{
+        width:100vh;
+        height:900px;
+    }
 
 `
