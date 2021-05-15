566ff55fbcc09e8af2425cf59b19ae214af2e0cc
@fausto1 <fausto@parts-badger.com>
2021-05-14 19:08:02 -0500

    initial commit

--- styles/pose_estimation.styles.tsx
+++ styles/pose_estimation.styles.tsx
@@ -2,9 +2,7 @@ import Styled from "styled-components"
 
 export const PageWrapper = Styled.div`
 
-    display: grid;
-    grid-template-columns: 100%;
-    justify-items: center;
+    display: block;
     background-color: #FFD733;
     width:100vh;
     height:900px;
