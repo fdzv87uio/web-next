7b702e97d62ab629d1aaa4af4eff3bdb54e23ff7
@fausto1 <fausto@parts-badger.com>
2021-05-14 20:09:06 -0500

    initial commit

--- styles/pose_estimation.styles.tsx
+++ styles/pose_estimation.styles.tsx
@@ -3,9 +3,8 @@ import Styled from "styled-components"
 export const PageWrapper = Styled.div`
 
     display: block;
-    margin: 0;
     background-color: #FFD733;
-    width:100%;
+    width:100vh;
     height:900px;
     
     video{
